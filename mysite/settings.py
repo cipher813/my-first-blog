ecb64adb303098648f9bad2ab3d9dce09594b814
Brian McMahon <cipher813@gmail.com>
2017-10-23 09:47:08 +0630

    finalizing blog for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'cipher813',
+        'PASSWORD':'cipher813',
+        'HOST':'localhost',
+        'PORT': '',
     }
 }
 
@@ -121,3 +125,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
